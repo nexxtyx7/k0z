5e25f11b35990d10385cfaf99549c72f7ca95ec0
nexxtyx7 <sae100157@gmail.com>
2026-09-20 21:41:15 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// COLOQUE AQUI os dados do seu projeto Supabase.
+// Nunca coloque a service_role key neste arquivo.
+window.SUPABASE_URL = 'COLE_AQUI_A_URL_DO_SEU_PROJETO';
+window.SUPABASE_ANON_KEY = 'COLE_AQUI_A_CHAVE_ANON_PUBLICA';
